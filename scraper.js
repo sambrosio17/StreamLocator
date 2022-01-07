facb4ea66f627a784f5689c106d234e9b7f9bc5d
sambrosio17 <ambrosio.s@outlook.it>
2022-01-07 21:37:48 +0100

    testing proxy

--- scraper.js
+++ scraper.js
@@ -1,4 +1,4 @@
-const puppeteer=require ('puppeteer');
+const puppeteer=require ('puppeteer-extra');
 var browser;
 const PRIME = "https://www.primevideo.com/detail/"
 const startService = async () => {
