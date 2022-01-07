53b5631b76fd5290fd2a2b0cd5f9b1c164e81255
sambrosio17 <ambrosio.s@outlook.it>
2022-01-07 12:19:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const scraper = require('./scraper.js');
+const express = require('express');
+const app= express();
+const mongoose = require('mongoose');
+require('dotenv').config()
+const DB_PASS=process.env.DB_PASSWORD;
+console.log(DB_PASS);
+
+mongoose.connect("mongodb+srv://salvatore:"+DB_PASS+"@cluster0.yrroi.mongodb.net/test");
+const Item=mongoose.model("Item",{title:String, providers: JSON});
+
+app.get('/:movie', async function(req, res){
+    if(req.params.movie.includes("favicon")){
+        res.status(204);
+    }else {
+    const result= await Item.find({title: { '$regex' : req.params.movie, '$options' : 'i' }}).exec();
+    if(result.length==0){
+        console.log("crea entry nel db")
+        const scrapeRes= await scraper(req.params.movie,null);
+        const item= new Item({title: scrapeRes.title, providers:scrapeRes.providers});
+        const itemSaved= await item.save();
+        res.send(itemSaved);
+    }
+    await res.send(result);
+}
+});
+app.use(express.json());
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
+
+app.post('/request',async function(req, res) {
+    var error="";
+    var isError=false;
+    const body=req.body;
+    const title=body.title;
+    if(!title) error="Nessun Titolo inserito!"; isError=isError || true;
+    if(!error)
+    {
+        const dbRetrived= await Item.find({title: { '$regex' : title, '$options' : 'i' }}).exec();
+        if(dbRetrived.length!=0) error="Il titolo inserito è già presente!"; isError=isError || true;
+        if(!error)
+        {
+            const providers=body.providers;
+            const toSave=new Item({title: title, providers: providers});
+            res.send(toSave.save())
+        }
+        else{
+            res.send({"error": error});
+        }
+    }
+    else{
+        res.send({"error": error});
+    }
+    
+});
+
+app.get('/favicon.ico', function(req, res) { 
+    res.status(204);
+    res.end();    
+});
+
+app.get('/provider/:providerName',async function(req,res) {
+    if(req.params.providerName.includes("favicon")){
+        res.status(204);
+    }{
+    const providerName=req.params.providerName
+    const retrived= await Item.find({"providers.name" : {'$regex': providerName, '$options': 'i'}}).exec();
+    res.send(retrived)
+    }
+});
+
+
+
+const port= process.env.PORT || 3000;
+app.listen(port, console.log("working on port: "+port));
---
+++ scraper.js
@@ -0,0 +1,72 @@
+const puppeteer=require ('puppeteer');
+const scan=require('readline');
+const scanner=scan.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+var browser;
+const PRIME = "https://www.primevideo.com/detail/"
+const startService = async () => {
+    browser= await puppeteer.launch({headless:true,userDataDir:'./cookieHandler'});
+    const pageIn= await browser.newPage();
+    return pageIn;
+}
+
+const doSearch = async (keyword) => {
+    const page=await startService();
+    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36");
+    await page.goto("https://www.google.com/search?q="+keyword, { waitUntil: 'networkidle2' });
+    return page;
+}
+
+const consentCookie = async (page) => {
+    await page.$$eval(".QS5gu.sy4vM", el => el==null || el.length<1 ? console.log("nothing") : el[1].click());
+
+}
+const scrapeData = async (keyword, callback) => {
+    console.log(keyword);
+    var page= await doSearch(keyword);
+    await page.screenshot({path:'test.jpg'});
+    await consentCookie(page);
+    //await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36");
+    const browser= page.browser();
+    //await page.waitForTimeout(1000);
+    var title=await (await (await page.$(".SPZz6b span")).getProperty("innerText")).jsonValue();
+    var containers=await page.$$(".w6bhBd");
+    if(containers==null || containers.length<1){
+        containers=await page.$$(".o0DLIc.w6bhBd");
+        if(containers==null || containers.length<1)
+            console.log("no res")
+    }else {
+        console.log("results are in: ");
+    }
+    var urls= await page.$$(".sATSHe a[ping]");
+    //await console.log("url: "+ urls);
+    var result= {
+        title: title,
+        providers:[]
+    }
+    //nome del servizio
+//    await console.log( await (await (await containers[0].$(".ellip.bclEt")).getProperty("innerText")).jsonValue());
+    //await containers[0].$eval(".ZkkK1e.yUTMj.k1U36b" ,(el) => el.click());
+    //prime
+//  await console.log(await page.url().slice(204).split("%")[0]); 
+    for(var i=0; i<containers.length; i++){
+        var provider={
+            name: "",
+            url: ""
+        };
+        provider.name = await (await (await containers[i].$(".ellip.bclEt")).getProperty("innerText")).jsonValue();
+        provider.url= await (await urls[i+1].getProperty("href")).jsonValue();
+        //await console.log(provider);
+        await result.providers.push(provider);
+    }
+    data=result;
+    print();
+    browser.close();
+    return await result;
+}
+var data=null;
+const print = ()=> console.log(data);
+
+module.exports = scrapeData;
